a726da794bf9ada3ee9bedaf2058fae34f76e5df
Konrad <konrad.hajduga@gmail.com>
2021-02-01 00:02:46 +0100

    add class unit

--- Models/ClassesUnit.cs
+++ Models/ClassesUnit.cs
@@ -12,5 +12,7 @@ namespace PO_implementacja_StudiaPodyplomowe.Models
         public DateTime ClassEnding { get; set; }
         public string ClassroomNumber { get; set; }
         public ClassesForm ClassesForm { get; set; }
+        public Course ClassCourse { get; set; }
+        public Lecturer ClassLecturer { get; set; }
     }
 }
