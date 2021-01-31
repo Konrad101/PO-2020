4af053f35fef4182df1e7ab5420e957aabc7f3d2
Kamil <kogol99@gmail.com>
2021-01-31 01:25:11 +0100

    add new database structure

--- Models/Database/Sql/database_initialize.sql
+++ Models/Database/Sql/database_initialize.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Users (
   userId INT PRIMARY KEY,
-  userName VARCHAR(31),
+  name VARCHAR(31),
   surname VARCHAR(31),
   email VARCHAR(63),
   birthdate DATE,
@@ -15,7 +15,8 @@ CREATE TABLE Participants (
   secondName VARCHAR(31),
   pesel VARCHAR(12),
   phoneNumber VARCHAR(15),
-  mothersName VARCHAR(15),
+  birthdate DATE,
+  mathersName VARCHAR(15),
   fathersName VARCHAR(15),
   startDate DATE,
   endDate DATE,
@@ -37,83 +38,67 @@ CREATE TABLE StudyFieldManager (
   FOREIGN KEY (userId) REFERENCES Users (userId)
 );
 
+CREATE TABLE Editions (
+  edNumber INT PRIMARY KEY
+  managerId INT,
+  FOREIGN KEY (managerId) REFERENCES StudyFieldManager (managerId)
+);
 
-CREATE TABLE SubmittingTheses (
+-- formularz zg³oszenia
+CREATE TABLE SubmissionTheses (
   submissionId INT PRIMARY KEY,
-  lecturerId INT,
   thesisTopic VARCHAR(2048),
-  topicNumber INT, -- max 5
+  topicNumber INT, -- max 5 cyfr
   thesisObjectives VARCHAR(2048),
   thesisScope VARCHAR(2048),
-  FOREIGN KEY (lecturerId) REFERENCES Lecturers (lecturerId)
+  submissionStatus VARCHAR(15),
+  finalThesisId INT,
+  edNumber INT,
+  FOREIGN KEY (finalThesisId) REFERENCES FinalTheses (finalThesisId),
+  FOREIGN KEY (edNumber) REFERENCES Editions (edNumber)
 );
 
--- PU Kamil
-CREATE TABLE StudyFieldManagerSubmissions (
-  managerId INT,
-  submissionId INT,
-  FOREIGN KEY (managerId) REFERENCES StudyFieldManager (managerId),
-  FOREIGN KEY (submissionId) REFERENCES SubmittingTheses (submissionId)
-);
-
-
-CREATE TABLE FinalThesesForms (
+-- formularz recenzji pracy koñcowej
+CREATE TABLE FinalThesesReview (
   formId INT PRIMARY KEY,
-  thesisTopic VARCHAR(2048),
-  participantData VARCHAR(128),
   titleCompability VARCHAR(128),
   thesisStructureComment VARCHAR(128),
   newProblem VARCHAR(128),
   sourcesUse VARCHAR(128),
-  sourcesCharacteristics VARCHAR(256),
   formalWorkSide VARCHAR(256),
+  wayToUse VARCHAR(256),
   substantiveThesisGrade VARCHAR(2048),
-  thesisGrade VARCHAR(16),
-  formDate DATE
+  thesisGrade VARCHAR(15),
+  formDate DATE,
+  formStatus VARCHAR(15),
+  finalThesisId INT,
+  FOREIGN KEY (finalThesisId) REFERENCES FinalTheses (finalThesisId),
 );
-
--- PU Konrad
-CREATE TABLE LecturerThesesForms (
-  lecturerId INT,
-  formId INT,
-  FOREIGN KEY (lecturerId) REFERENCES Lecturers (lecturerId),
-  FOREIGN KEY (formId) REFERENCES FinalThesesForms (formId)
-);
-
-
-CREATE TABLE Editions (
-  number INT PRIMARY KEY
-);
-
 
 CREATE TABLE FinalExams (
   examId INT PRIMARY KEY,
   examDate DATE,
   classroom VARCHAR(15),
-  examCourse VARCHAR(127)
+  managerId INT,
+  FOREIGN KEY (managerId) REFERENCES StudyFieldManager (managerId)
 );
 
--- PU Konrad
-CREATE TABLE StudyFieldManagerExams (
-  managerId INT,
+CREATE TABLE Questions (
+  questionId INT PRIMARY KEY,
+  content VARCHAR(1023),
+  points INT,
+  answer VARCHAR(2047),
   examId INT,
-  FOREIGN KEY (managerId) REFERENCES StudyFieldManager (managerId),
   FOREIGN KEY (examId) REFERENCES FinalExams (examId)
 );
-
 
 CREATE TABLE Courses (
   courseId VARCHAR(15) PRIMARY KEY,
   courseName VARCHAR(63),
   ectsPoints INT,
-  semester INT
-);
-
-CREATE TABLE ParticipantsCourses (
-  participantId INT,
-  courseId VARCHAR(15),
-  FOREIGN KEY (participantId) REFERENCES Participants (participantId),
-  FOREIGN KEY (courseId) REFERENCES Courses (courseId)
+  semester INT,
+  edNumber INT,
+  FOREIGN KEY (edNumber) REFERENCES Editions (edNumber)
 );
 
 CREATE TABLE ClassesUnits (
@@ -121,11 +106,14 @@ CREATE TABLE ClassesUnits (
   classBeginning DATE,
   classEnding DATE,
   classroomNumber VARCHAR(31),
+  classForm INT,
   courseId VARCHAR(15),
-  FOREIGN KEY (courseId) REFERENCES Courses (courseId)
+  lecturerId INT,
+  FOREIGN KEY (courseId) REFERENCES Courses (courseId),
+  FOREIGN KEY (lecturerId) REFERENCES Lecturers (lecturerId)
 );
 
--- PU Kamil i Radek
+-- obecnoœci studentów na zajeciach
 CREATE TABLE Attendances (
   participantId INT,
   classUnitId INT,
@@ -133,58 +121,37 @@ CREATE TABLE Attendances (
   FOREIGN KEY (classUnitId) REFERENCES ClassesUnits (classUnitId)
 );
 
-
-CREATE TABLE PartialGrades (
+-- ocena cz¹stkowa
+CREATE TABLE PartialCourseGrades (
   partialGradeId INT PRIMARY KEY,
   gradeDate DATE,
-  gradeValue FLOAT,
-  comment VARCHAR(255)
+  gradeValue VARCHAR(15),
+  participantGradeListId INT,
+  comment VARCHAR(255),
+  FOREIGN KEY (participantGradeListId) REFERENCES ParticipantGradeList (participantGradeListId),
 );
 
--- PU Radek
+-- po³¹czenie miêdzy uczestnik - kurs
 CREATE TABLE ParticipantsGrades (
 	participantId INT,
 	courseId VARCHAR(15),
-	partialGradeId INT,
+	participantGradeListId INT,
 	FOREIGN KEY (participantId) REFERENCES Participants (participantId),
 	FOREIGN KEY (courseId) REFERENCES Courses (courseId),
-	FOREIGN KEY (partialGradeId) REFERENCES PartialGrades (partialGradeId)
+	FOREIGN KEY (participantGradeListId) REFERENCES ParticipantGradeList (participantGradeListId)
 );
 
-
-CREATE TABLE QuestionGrades (
-  questionGradeId INT PRIMARY KEY,
-  questionGrade FLOAT
-);
-
+-- lista ocen uczestnika z kursu
+CREATE TABLE ParticipantGradeList (
+	participantGradeListId INT PRIMARY KEY,
+)
 
 CREATE TABLE FinalTheses (
   finalThesisId INT PRIMARY KEY,
-  participantId INT,
-  lecturerId INT,
   deliveryDeadline DATE,
-  topic VARCHAR(127),
-  comments VARCHAR(255),
-  ifTopicApproved TINYINT,
-  ifDeclarationOfIndependentThesis TINYINT,
-  ifDeclarationOfShareThesis TINYINT,
-  ifDeclarationOfIdentityThesis TINYINT,
-  FOREIGN KEY (participantId) REFERENCES Participants(participantId),
-  FOREIGN KEY (lecturerId) REFERENCES Lecturers(lecturerId)
-);
-
-CREATE TABLE ThesisComments (
-  finalThesisId INT,
-  comment VARCHAR(255),
-  FOREIGN KEY (finalThesisId) REFERENCES FinalTheses (finalThesisId)
+  participantId INT,
+  FOREIGN KEY (participantId) REFERENCES Participants (participantId)
 );
 
 
-CREATE TABLE Questions (
-  questionId INT PRIMARY KEY,
-  content VARCHAR(1023),
-  points INT,
-  answer VARCHAR(2047)
-);
-
-
+SELECT * FROM FinalExams FE JOIN StudyFieldManager SFM ON FE.managerId = SFM.managerId JOIN Editions ED ON ED.managerId = SFM.managerId WHERE ED.number = 0 AND SFM.managerId = 0
