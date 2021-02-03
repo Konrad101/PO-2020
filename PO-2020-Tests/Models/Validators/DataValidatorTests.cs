800a6d78c59a4b34ffb0a68efeeedc62b34fd073
Konrad <konrad.hajduga@gmail.com>
2021-02-03 16:39:16 +0100

    add tests

---
+++ PO-2020-Tests/Models/Validators/DataValidatorTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework.Internal;
+using PO_implementacja_StudiaPodyplomowe.Models.Validators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PO_implementacja_StudiaPodyplomowe.Models.Validators.Tests
+{
+    [TestClass()]
+    public class DataValidatorTests
+    {
+        [TestMethod()]
+        public void DateIsValidTest()
+        {
+            Assert.IsTrue(DataValidator.DateIsValid("31-01-2020"));
+            Assert.IsFalse(DataValidator.DateIsValid("32.01.2020"));
+            Assert.IsTrue(DataValidator.DateIsValid("31.01.2020"));
+            Assert.IsFalse(DataValidator.DateIsValid("29.02.2021"));
+            Assert.IsFalse(DataValidator.DateIsValid("01.02.2023"));
+        }
+
+
+        [TestMethod()]
+        public void NumberIsValidTest()
+        {
+            Assert.IsFalse(DataValidator.NumberIsValid(""));
+            Assert.IsFalse(DataValidator.NumberIsValid(" "));
+        }
+    }
+}
